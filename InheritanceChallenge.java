9675e7f4eee7404c8628e0c1c6921b49eaa83ad0
Christian Benjaminsson <54481384+Christianbenjaminsson@users.noreply.github.com>
2019-10-23 08:48:05 +0200

    Added method in Outlander and som examples to run main

--- InheritanceChallenge.java
+++ InheritanceChallenge.java
@@ -3,5 +3,11 @@ package com.example.inheritancechallenge;
 public class InheritanceChallenge {
     public static void main(String[] args) {
 
+        Outlander outlander = new Outlander(36);
+        outlander.steer(45);
+        outlander.accelerate(30);
+        outlander.accelerate(20);
+        outlander.accelerate(-42);
+
     }
 }
